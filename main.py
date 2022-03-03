39c0478479784b01a36fbe6575bfe155ee870f4d
jonpage3 <jonpage3@gmail.com>
2022-03-02 20:23:33 -0500

    adding new cleaned csv dataset

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import csv
 
 master_list = []
-with open('ps3000_lsc_3-2.csv',encoding="utf-8" ) as csvfile:
+with open('ps3500-pt-testset.csv',encoding="utf-8-sig" ) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         master_list.append(row)
