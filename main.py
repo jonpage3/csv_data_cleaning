36abcbb86cdafa5a961e19c93eddd5980fed1eb3
jonpage3 <jonpage3@gmail.com>
2022-03-02 21:00:25 -0500

    edits to main.py, logic added. closer to cleaning length

--- main.py
+++ main.py
@@ -7,4 +7,30 @@ with open('ps3500-pt-testset.csv',encoding="utf-8-sig" ) as csvfile:
     for row in reader:
         master_list.append(row)
 
-print(master_list)
+#clean length field to just produce number of pages
+def clean_length(book_list):
+    #if 'pages' in field value process
+    #else put default 100 value
+    for item in book_list:
+        if 'pages' in item['length']:
+            print(item['length'])
+        else:
+            item['length'] = 100
+
+
+#clean height field to just produce number in cm
+def clean_height(list):
+    pass
+
+#clean date to produce year data xxxx
+def clean_date(list):
+    pass
+
+#write the list into a csv file
+def write_csv(list):
+    pass
+
+#clean_date(master_list)
+#clean_height(master_list)
+clean_length(master_list)
+#write_csv(master_list)
